f33147b2a2ea8c12ddf8aa9b14014ae3cce6d2cc
chaofan.zhang <chaofan.zhang@zhangmen.com>
2020-03-18 17:42:44 +0800

    自动注入

--- src/main/java/com/learn/spring/autowired/AutowiredInject.java
+++ src/main/java/com/learn/spring/autowired/AutowiredInject.java
@@ -2,6 +2,7 @@ package com.learn.spring.autowired;
 
 import com.learn.design.strategy.Strategy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -15,12 +16,26 @@ import java.util.Map;
  **/
 @Component
 public class AutowiredInject {
+    /**
+     * 静态属性 用set方法注入
+     */
+    private static String demo;
 
+    /**
+     * 自动注入实现接口类
+     */
     @Autowired
     Map<String, Strategy> strategyInterfaceMap;
 
     @PostConstruct
     public void init() {
         System.out.println("StrategyInterface = " + strategyInterfaceMap);
+        System.out.println("init demo = " + demo);
+    }
+
+    @Value("${demo}")
+    public void setDemo(String demo) {
+        AutowiredInject.demo = demo;
+        System.out.println("demo = " + demo);
     }
 }
